1c50b2c55b6d1d8f3375805df22772338bd986d6
Mike Agbelou <mike.agbelou@gmail.com>
2024-03-03 17:22:02 +0100

    config ronte controller (index) and add homme Controller

--- public/index.php
+++ public/index.php
@@ -1,9 +1,21 @@
 <?php
 
-use App\Kernel;
-
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
+use App\Kernel;
+use Symfony\Component\HttpFoundation\Request;
+
+
 return function (array $context) {
-    return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
+    // Create an instance of Symfony core
+    $kernel = new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
+
+    // Manage the request via the kernel
+    $request  = Request::createFromGlobals();
+    $response = $kernel->handle($request);
+    $response->send();
+    $kernel->terminate($request, $response);
+
+    // You can return something here if necessary
+    // return $someValue;
 };
